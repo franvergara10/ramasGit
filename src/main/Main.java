42fc72b1e4e2c4e1c73c4ad50f31c5f19e38bad6
franvergara10 <vergarafran12345@gmail.com>
2025-02-05 14:19:47 +0100

    x

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,6 +27,7 @@ public class Main {
 			
 			switch(opcion) {
 			case 1:
+				System.out.println("pasofvr");
 				System.out.println("-------------------------------------------");
 				System.out.println();
 				break;
