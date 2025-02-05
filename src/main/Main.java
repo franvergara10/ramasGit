800b887c762e1c0a6b0fac7e162f122532025964
miguelGonzalez <mgonpor116@g.educaand.es>
2025-02-05 14:08:44 +0100

    update main

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,6 +27,7 @@ public class Main {
 			
 			switch(opcion) {
 			case 1:
+				System.out.println("paso 2");
 				System.out.println("-------------------------------------------");
 				System.out.println();
 				break;
