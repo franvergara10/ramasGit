173b625ef1c63bf507b48ecaf09b604625a88c39
Raquelfefer <raquelfefer@gmail.com>
2025-02-05 14:08:11 +0100

    añadido paso 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,6 +27,7 @@ public class Main {
 			
 			switch(opcion) {
 			case 1:
+				System.out.println("Paso 1");
 				System.out.println("-------------------------------------------");
 				System.out.println();
 				break;
